22e4ee7004051f6e06149b49beef060e32998ead
argajienbi <armin.gandi@gmail.com>
2019-01-08 17:34:31 +0700

    Update user.php

--- user.php
+++ user.php
@@ -12,3 +12,19 @@ we_stat = true;
   $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
   $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
   $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo." 73jqem    ".$t;
+echo  $ red . " [!] peringatan !!! " . $ t ;
+echo  $ kuning . " [-] ini adalah program ilegal " . $ t ;
+echo  $ kuning . " [-] Resiko sepenuhnya di tanggung pengguna " . $ t . $ ijo . $ t ;
+$ sungging  =  $ putih . ' [*] ' . $ ijo . ' berlangganan youtube sungging ' . $ putih . ' [-] kode: ' . $ ijo . ' L41023 ' ;
+$ ads =  $ putih . " [÷] saluran resmi [÷] " . $ t . $ ijo . " [-] adi bordir " . $ putih . " | atau | " . $ ijo . " sungging [-] " ;
+$ msgtoto  =  " [!] Berlangganan Youtube adi bordir dan Sungging " . $ t . $ red . " [°] demi keamanan hentikan aktifitas bot sekarang !!!! " . $ t . $ kuning . " [•] ctrl + c " ;
+$ msgyd  =  " [!] ganguan pada server jangan panik ini tidak lama " ;
+$ yd_stat  =  true ;
+$ vip1 =  false ;
+// $ weblock = [""];
+$ ydblock  = [ " " ];
+$ we_stat  =  false ;
+$ msgwe  =  " [!] oops maaf, mohon periksa billboard dengan e " . $ t . $ red . " [•] demi keamanan hentikan aktivitas ini   " . $ t . $ kuning . " [-] ctrl + c kalo ngeyel bodo amat " . $ t . $ turki . " [•] pahami dengan baik jangan banyak tanya " ;
+$ msgveeu  =  " [!] nikmati hidup Anda " ;
+$  data  = [
+'Mutia Amelia' , 'Ana Maryanah'] ;
