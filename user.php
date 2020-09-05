4a84d279496020a2347f95d561ce183001c21447
argajienbi <armin.gandi@gmail.com>
2020-09-05 20:15:16 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,30 +1,16 @@
 <?php
-$stat_sseth=true;
-$msgsseth= "[---] enjoy your life";
-$stat_cp =true;
-$msgcp = "[--] enjoy your life";
-$figlet= $ijo."
-we_stat = true;
-2
-── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ " . $ Kuning . "  [`|| _ _.       
-── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
-── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
-  $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
-  $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
-  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo." 73jqem    ".$t;
-echo  $ red . " [!] peringatan !!! " . $ t ;
-echo  $ kuning . " [-] ini adalah program ilegal " . $ t ;
-echo  $ kuning . " [-] Resiko sepenuhnya di tanggung pengguna " . $ t . $ ijo . $ t ;
-$ sungging  =  $ putih . ' [*] ' . $ ijo . ' berlangganan youtube sungging ' . $ putih . ' [-] kode: ' . $ ijo . ' L41023 ' ;
-$ ads =  $ putih . " [÷] saluran resmi [÷] " . $ t . $ ijo . " [-] adi bordir " . $ putih . " | atau | " . $ ijo . " sungging [-] " ;
-$ msgtoto  =  " [!] Berlangganan Youtube adi bordir dan Sungging " . $ t . $ red . " [°] demi keamanan hentikan aktifitas bot sekarang !!!! " . $ t . $ kuning . " [•] ctrl + c " ;
-$ msgyd  =  " [!] ganguan pada server jangan panik ini tidak lama " ;
-$ yd_stat  =  true ;
-$ vip1 =  false ;
-// $ weblock = [""];
-$ ydblock  = [ " " ];
-$ we_stat  =  false ;
-$ msgwe  =  " [!] oops maaf, mohon periksa billboard dengan e " . $ t . $ red . " [•] demi keamanan hentikan aktivitas ini   " . $ t . $ kuning . " [-] ctrl + c kalo ngeyel bodo amat " . $ t . $ turki . " [•] pahami dengan baik jangan banyak tanya " ;
-$ msgveeu  =  " [!] nikmati hidup Anda " ;
-$  data  = [
-'Mutia Amelia' , 'Ana Maryanah'] ;
+echo$t;
+echo $tt.$ijo."╔═══╗╔═══╗╔═══╗╔══╗╔═╗─╔╗╔═══╗
+║╔═╗║║╔═╗║║╔═╗║╚╣─╝║║╚╗║║║╔═╗║
+║║─╚╝║║─║║║╚═╝║─║║─║╔╗╚╝║║║─╚╝
+║║─╔╗║╚═╝║║╔══╝─║║─║║╚╗║║║║╔═╗
+║╚═╝║║╔═╗║║║───╔╣─╗║║─║║║║╚╩═║
+╚═══╝╚╝─╚╝╚╝───╚══╝╚╝─╚═╝╚═══╝".$tt;
+
+echo$t.$putih."[*]".$ijo." BOT CAPING ".$putih." [ ".$biru."✓ ".$putih."]".$t.$t;
+echo$putih."[#]".$ijo." Creator : ".$pink." Armin Gandi".$t.$t;
+
+
+
+
+
